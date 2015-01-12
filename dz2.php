5eb786d1d64f9cf8189874fb3bb2f5a503eccea8
Andrew Kosenkov <andrewkosenkov@gmail.com>
2015-01-12 13:08:35 +0300

    firs commit

---
+++ dz2.php
@@ -0,0 +1,7 @@
+<?
+header('Content-type: text/html; charset=utf-8');
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+ <?
+header('Content-type: text/html; charset=utf-8');
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors','1');
+#    echo $x;
+#    prent ('пока как дела?');
+$x='человек';
+echo "Привет \n" .$x." как дела?";
+
+
+?>
